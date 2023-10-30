a83d08ab67b6b880988810af79813fbf20de822d
vikash <vikash.t@karza.in>
2023-10-30 11:24:02 +0530

    initial commit

---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'Restraunt app',
+    description: "Using React i have made a Restraunt app - from start to finish. This app allows public to go through menu of the restraunt, info about the location and book a table or for takeaway orders.",
+      image: '/images/1.png',
+      tags: ['JavaScript', 'HTML', 'React', 'CSS'],
+    Code: 'https://github.com/himanshubhadoria2727/Restaurant_app',
+    visit: 'https://restaurant-app-22d61.web.app/',
+    id: 0,
+  },
+  {
+    title: 'E-Commerce',
+    description:"Made an E-commerce website using React. Its a mere replica of Amazon with functionality like add to card , remove from cart , subtotal , User login etc.",
+    image: '/images/2.png',
+    tags: ['React', 'JavaScript', 'CSS'],
+    Code: 'https://github.com/himanshubhadoria2727/Amazon-Clone',
+    visit: 'https://amazn-ecommerce.netlify.app/',
+    id: 1,
+  },
+  // {
+  //   title: 'WebRTC App',
+  //   description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+  //     image: '/images/3.jpg',
+  //     tags: ['React', 'WebRTC'],
+  //   Code: 'https://google.com',
+  //   visit: 'https://google.com',
+  //   id: 2,
+  // },
+  // {
+  //   title: 'Unichat',
+  //   description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+  //   image: '/images/4.jpg',
+  //   tags: ['React', 'ChatEngine', 'Firebase'],
+  //   source: 'https://google.com',
+  //   visit: 'https://google.com',
+  //   id: 3,
+  // },
+];
+
+export const TimeLineData = [
+  { year: 2019, text: 'Graduated from Army Public Scool, Gwalior', },
+  { year: 2021, text: 'Completed an internship in The Sparks Foundation as a data science and business analyst', },
+  { year: 2021, text: 'Completed an internship Internshala on machince learning', },
+  { year: 2022, text: 'Minor degree in Data Science', },
+  { year: 2023, text: 'Graduated from Amity University, Gwalior, M.p', },
+];
